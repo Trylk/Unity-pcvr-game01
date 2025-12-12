e5cf0a264d9832bc19f173b1e4e09cdb580ef1ec
Trylk <dubes96436845@gmail.com>
2025-12-12 13:46:24 -0500

    Camera following mouse

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,7 +4,11 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 6f;
     public float jumpHeight = 1.8f;
-    public float gravity = -18f; // stronger gravity = better game feel
+    public float gravity = -18f;
+
+    [Header("Mouse Look")]
+    public float mouseSensitivity = 2.5f;
+    public float maxLookAngle = 80f;
 
     [Header("Lean Settings")]
     public Transform cameraPivot;
@@ -13,16 +17,48 @@ public class PlayerController : MonoBehaviour
 
     private CharacterController controller;
     private Vector3 velocity;
+
+    private float xRotation = 0f;
     private float currentLean = 0f;
     private float targetLean = 0f;
 
     void Start()
     {
         controller = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
+        // ----- MOUSE LOOK -----
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 100f * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * 100f * Time.deltaTime;
+
+        // Horizontal turn (player)
+        transform.Rotate(Vector3.up * mouseX);
+
+        // Vertical look (camera)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -maxLookAngle, maxLookAngle);
+
+        // ----- LEAN INPUT -----
+        if (Input.GetKey(KeyCode.Q))
+            targetLean = leanAngle;
+        else if (Input.GetKey(KeyCode.E))
+            targetLean = -leanAngle;
+        else
+            targetLean = 0f;
+
+        currentLean = Mathf.Lerp(currentLean, targetLean, Time.deltaTime * leanSpeed);
+
+        if (cameraPivot != null)
+        {
+            cameraPivot.localRotation =
+                Quaternion.Euler(xRotation, 0f, currentLean);
+        }
+
         // ----- MOVEMENT -----
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
@@ -46,18 +82,5 @@ public class PlayerController : MonoBehaviour
         // ----- GRAVITY -----
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-
-        // ----- LEAN -----
-        if (Input.GetKey(KeyCode.Q))
-            targetLean = leanAngle;
-        else if (Input.GetKey(KeyCode.E))
-            targetLean = -leanAngle;
-        else
-            targetLean = 0f;
-
-        currentLean = Mathf.Lerp(currentLean, targetLean, Time.deltaTime * leanSpeed);
-
-        if (cameraPivot != null)
-            cameraPivot.localRotation = Quaternion.Euler(0f, 0f, currentLean);
     }
 }
